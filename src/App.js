7f923cbab946c10f621370c5b9655ca3092122ea
4m4nu4l4 <65728939+4m4nu4l4@users.noreply.github.com>
2024-02-29 21:37:05 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import BuscaCep from "./components/BuscaCep";
 import MudarTema from "./components/MudarTema";
-import "./config/scss/styles.scss";
-
+import './config/scss/style.scss'
 export default function App() {
   return (
     <div>
